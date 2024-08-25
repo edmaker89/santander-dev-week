9b66596a27db6ecaa4cca6e817272efb5f91950a
edmaker <edmaker@gmail.com>
2024-08-25 11:19:10 -0300

    models e repository

--- src/main/java/app/domain/repository/UserRepository.java
+++ src/main/java/app/domain/repository/UserRepository.java
@@ -4,6 +4,7 @@ import app.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
