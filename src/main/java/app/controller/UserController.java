abc82e8f4aba5a9fcb9b7bc347620a780fa2753f
edmaker <edmaker@gmail.com>
2024-08-25 12:29:03 -0300

    feat: GlobalExceptionHandler.java

---
+++ src/main/java/app/controller/UserController.java
@@ -0,0 +1,35 @@
+package app.controller;
+
+import app.domain.model.User;
+import app.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        var user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user){
+        var userCreate = userService.create(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreate.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreate);
+    }
+}
